922da759bc60d949f68e0816491c883f7ade2f61
raedeid <raededialrfou12345@gmail.com>
2019-11-14 00:41:58 +0200

    added property to css

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 'use strict'
 var score = 0;
-
-function Q1_5(){
+function Q1_5() {
     var point = 0;
     var user_name = prompt('whats your name ?');
     alert(' welcome to my website ' + '' + user_name);
+    var correct_answer = [['yes', 'y'], ['no', 'n'], 'yes'];
     var question = ["Do you think i love sport ?", 'Do you think i am work hard ?', 'Do you think i hate summer ?', 'do you think i like music ?', 'do you think i love movies'];
     var answer_yes = ['right,specialy basketball', 'work make me better', 'i love winter', 'all time listen to music', 'specially crime type' + user_name];
     var answer_no = ['wrong answer', 'life needed it wrong answer', 'i hate summer', 'i like music with all type', 'who dont like movies' + user_name];
     for (var i = 0; i < question.length; i++) {
         var response = prompt(question[i]).toLowerCase();
         while (response != 'yes' && response != 'no' && response != 'y' && response != 'n') {
-            console.log(prompt(question[i]));
+            // console.log(prompt(question[i]));
             response = prompt(question[i]).toLowerCase();
         }
         if (response == 'yes' || response == 'y') {
-            // console.log(answer_yes[i]);
+            //console.log(answer_yes[i]);
             point++;
             alert('correct answer ' + answer_yes[i] + ' ' + ' your score is' + point.toString());
         } else if (response == 'no' || response == 'n') {
-            // console.log(answer_no[i]);
+            //console.log(answer_no[i]);
             alert('wrong answer ' + answer_no[i] + ' ' + ' your score is ' + point.toString());
         }
-    }return point
-} 
+    } return point
+}
 score = score + Q1_5()
-function Q6(){
+function Q6() {
     var point = 0;
     var guess = prompt('guess my lucky number')
-    var r = 0
-    while (guess != '9' && r < 3) {
+    var iteration = 0
+    while (guess != '9' && iteration < 3) {
         if (parseInt(guess) > 9) {
             alert('your answer is higher , try again');
             guess = prompt('guess my lucky number');
@@ -37,24 +37,24 @@ function Q6(){
             alert('your answer is lower , try again');
             guess = prompt('guess my lucky number');
         }
-        r++;
+        iteration++;
     }
-    if (r == 3) {
+    if (iteration == 3) {
         alert('the right answer is 9');
     } else if (guess == '9') {
         point++;
         alert('congratulation you guess my lucky number is 9 the number your score is ' + ' ' + score.toString());
-    }return point
-}   
+    } return point
+}
 score = score + Q6()
 
-function Q7(){
-    var w = 0
+function Q7() {
+    var iteration = 0
     var point = 0;
     var wrong_answer = true;
     var prime = [11, 13, 17, 19, 23, 29];
     var answer_prime = prompt('give me prime number upper 10 and under 30');
-    while (w < 6 && wrong_answer) {
+    while (iteration < 6 && wrong_answer) {
         for (var G = 0; G < 6; G++) {
             if (prime[G] == answer_prime) {
                 point++;
@@ -62,14 +62,14 @@ function Q7(){
                 wrong_answer = false;
             }
         }
-        if (wrong_answer && w < 5) {
+        if (wrong_answer && iteration < 5) {
             alert('wrong answer , try again');
             answer_prime = prompt('give me prime number upper 10');
-        } else if (w > 5 && wrong_answer) {
+        } else if (iteration > 5 && wrong_answer) {
             alert('you loose all apportunity , sorry the right answer is ' + prime.join(','));
         }
-        w++;
-    }return point
+        iteration++;
+    } return point
 }
 score = score + Q7()
-    alert('your score is ' + score.toString() + ' from 7');
+alert('your score is ' + score.toString() + ' from 7');
